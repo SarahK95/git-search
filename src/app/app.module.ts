da781a1c66b7bfd3b2ba9c45a9f90d3090b2de1f
SarahKamunya <sarahkamunya254@gmail.com>
2022-04-19 07:35:50 +0300

    add git results folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { GitResultsComponent } from './git-results/git-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    GitResultsComponent
   ],
   imports: [
     BrowserModule,
