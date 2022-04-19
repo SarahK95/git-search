be6f970d20395a98fa1aa855376ecfb6c6fba793
SarahKamunya <sarahkamunya254@gmail.com>
2022-04-19 04:40:46 +0300

    add search-form

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/search-form/search-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.css']
+})
+
+
+
+export class SearchFormComponent implements OnInit {
+ 
+  // user!:User;
+  // username!: string;
+
+
+  // submitUsername(){
+    
+
+  // }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
