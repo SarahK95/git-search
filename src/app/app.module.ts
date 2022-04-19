94df78839ccedd32815755b02a3f444b7a93c11c
SarahKamunya <sarahkamunya254@gmail.com>
2022-04-19 08:49:54 +0300

    add days-count pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { GitResultsComponent } from './git-results/git-results.component';
+import { DaysCountPipe } from './days-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
-    GitResultsComponent
+    GitResultsComponent,
+    DaysCountPipe,
+  
   ],
 
 
@@ -21,7 +25,7 @@ import { GitResultsComponent } from './git-results/git-results.component';
     FormsModule
   ],
 
-  
+
   providers: [],
   bootstrap: [AppComponent]
 })
