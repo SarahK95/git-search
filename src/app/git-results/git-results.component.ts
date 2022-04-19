81342150f06671015761e911a7777c42a34a20e9
SarahKamunya <sarahkamunya254@gmail.com>
2022-04-19 07:49:26 +0300

    add changes in gitresultcomponent class

--- src/app/git-results/git-results.component.ts
+++ src/app/git-results/git-results.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SearchGithubService } from '../search-github.service';
+import { User } from '../user';
+
 
 @Component({
   selector: 'app-git-results',
   templateUrl: './git-results.component.html',
   styleUrls: ['./git-results.component.css']
 })
+
+
 export class GitResultsComponent implements OnInit {
 
+  user!:User; 
+  repoDetails = []; 
+  searchGithubService!:SearchGithubService;
+  
+
   constructor() { }
+
+  @Output() toggleBack = new EventEmitter();
 
   ngOnInit(): void {
   }
